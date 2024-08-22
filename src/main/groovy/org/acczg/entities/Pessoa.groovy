e1dfa59996d42b8b1cae5c701ec66b664f5e8909
vhprestes <vhprestes@gmail.com>
2024-08-22 11:22:44 -0300

    feat: implements abstract class Pessoa. This class contains all commom methods for his children class PessoaFisica and PessoaJuridica

---
+++ src/main/groovy/org/acczg/entities/Pessoa.groovy
@@ -0,0 +1,8 @@
+package org.acczg.entities
+
+import org.acczg.models.IPessoa
+
+abstract  class Pessoa implements IPessoa {
+
+//    TODO: métodos comuns de pessoa fisica e pessoa juridica ficarão aqui
+}
