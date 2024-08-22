4ea3d36c127ff8d4ec6d9cc58f63761c0bbf5885
vhprestes <vhprestes@gmail.com>
2024-08-22 13:16:47 -0300

    feat: cadastroCandidatos is now working

--- src/main/groovy/org/acczg/Main.groovy
+++ src/main/groovy/org/acczg/Main.groovy
@@ -95,7 +95,32 @@ void listarEmpresas(List<PessoaJuridica> empresas) {
 }
 
 void cadastroCandidatos(candidatos) {
+    println "Digite o nome do candidato: "
+    String nome = System.in.newReader().readLine()
+    println "Digite o email do candidato: "
+    String email = System.in.newReader().readLine()
+    println "Digite a descrição do candidato: "
+    String descricao = System.in.newReader().readLine()
+    println "Digite o CEP do candidato: "
+    String CEP = System.in.newReader().readLine()
+    println "Digite o estado do candidato: "
+    String estado = System.in.newReader().readLine()
+    println "Digite as competências do candidato, separadas por virgula: "
+    List<String> competencias = System.in.newReader().readLine().split(",")
+    println "Digite o CPF do candidato: "
+    String cpf = System.in.newReader().readLine()
+    println "Digite a idade do candidato: "
+    Integer idade = System.in.newReader().readLine().toInteger()
 
+    PessoaFisica candidato = new PessoaFisica(nome:nome, email:email, descricao:descricao, CEP:CEP, estado:estado, competencias:competencias, cpf:cpf, idade:idade)
+    candidatos.add(candidato)
+    println "Deseja cadastrar outro candidato? (S/N)"
+    String resposta = System.in.newReader().readLine()
+    if (resposta == "S") {
+        cadastroCandidatos(candidatos)
+    } else {
+        println "Candidato cadastrado com sucesso! Voltando ao menu principal..."
+    }
 }
 
 
