44bc0ffe1f41ab61d6db659206722c30dfc895e3
vhprestes <vhprestes@gmail.com>
2024-08-22 12:14:28 -0300

    feat: implements candidatos list

--- src/main/groovy/org/acczg/Main.groovy
+++ src/main/groovy/org/acczg/Main.groovy
@@ -1,8 +1,21 @@
 package org.acczg
 
-//import org.acczg.entities.PessoaFisica
+import org.acczg.entities.PessoaFisica
 
 static void main(String[] args) {
+
+    List<PessoaFisica> candidatos = []
+    PessoaFisica pessoaFisica = new PessoaFisica(nome:"João", email:"joao@email.com", descricao:"Desenvolvedor", CEP:"16600000", estado:"SP", competencias:["Java", "Groovy"], cpf:"12345678900", idade:30)
+    candidatos.add(pessoaFisica)
+    PessoaFisica pessoaFisica2 = new PessoaFisica(nome:"Elis", email:"elis@elis.com", descricao:"Analista de Sistemas", CEP:"54321000", estado:"RJ", competencias:["Java", "C"], cpf:"09876543211", idade:28)
+    candidatos.add(pessoaFisica2)
+    PessoaFisica pessoaFisica3 = new PessoaFisica(nome:"Victor", email:"victor@lindo.com", descricao:"Desenvolvedor", CEP:"26600000", estado:"RJ", competencias:["Javascript", "Java", "Groovy", "C#", "Python"], cpf:"11223344556", idade:35)
+    candidatos.add(pessoaFisica3)
+    PessoaFisica pessoaFisica4 = new PessoaFisica(nome:"Brenda", email:"brenda@bonita.com", descricao:"Professora", CEP:"46600000", estado:"RJ", competencias:["Java", "Groovy"], cpf:"22334455667", idade:32)
+    candidatos.add(pessoaFisica4)
+    PessoaFisica pessoaFisica5 = new PessoaFisica(nome:"Leandro", email:"leleco@email.com", descricao:"Desenvolvedor", CEP:"56600000", estado:"RJ", competencias:["Unity", "C++"], cpf:"33445566778", idade:29)
+    candidatos.add(pessoaFisica5)
+    List<String> empresas = []
 
 
     println "Escolha uma opção: "
