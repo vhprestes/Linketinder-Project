4a707554506352a18377048c8e6d443593623270
vhprestes <vhprestes@gmail.com>
2024-08-22 12:12:16 -0300

    feat: implements attributes to Pessoa.groovy

--- src/main/groovy/org/acczg/entities/Pessoa.groovy
+++ src/main/groovy/org/acczg/entities/Pessoa.groovy
@@ -3,6 +3,11 @@ package org.acczg.entities
 import org.acczg.models.IPessoa
 
 abstract  class Pessoa implements IPessoa {
-
+    String nome
+    String email
+    String descricao
+    String CEP
+    String estado
+    List<String> competencias
 //    TODO: métodos comuns de pessoa fisica e pessoa juridica ficarão aqui
 }
