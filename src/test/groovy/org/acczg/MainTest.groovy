330cece4d5662bd691740277b94e5f4e16054566
vhprestes <vhprestes@gmail.com>
2024-08-27 15:25:39 -0300

    feat: create tests structure 🧱

---
+++ src/test/groovy/org/acczg/MainTest.groovy
@@ -0,0 +1,10 @@
+package org.acczg
+
+import spock.lang.Specification
+
+class MainTest extends Specification {
+    def "test main method"() {
+        expect:
+        true == true // Replace with actual tests
+    }
+}
