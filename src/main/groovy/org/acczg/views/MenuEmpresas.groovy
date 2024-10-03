1b64b922b1203732279d48e4833e14e54270028e
vhprestes <vhprestes@gmail.com>
2024-10-03 15:25:17 -0300

    feat: menuEmpresas created (its the same from candidatos, w/ the fixes and all)

--- src/main/groovy/org/acczg/views/MenuEmpresas.groovy
+++ src/main/groovy/org/acczg/views/MenuEmpresas.groovy
@@ -1,4 +1,63 @@
 package org.acczg.views
 
+import org.acczg.controller.ControllerEmpresa
+import org.acczg.utils.DivisorLinha
+
 class MenuEmpresas {
+
+    static boolean menuEmpresa() {
+        ControllerEmpresa controllerEmpresa = new ControllerEmpresa()
+        Boolean menuON = true
+        String menuEmpresa = "Por favor escolha uma opção:\n" +
+                "1. Listar Empresas\n" +
+                "2. Cadastrar Empresa\n" +
+                "3. Alterar Empresa\n" +
+                "4. Excluir Empresa\n" +
+                "0. Voltar ao menu anterior"
+
+        Scanner scanner = new Scanner(System.in)
+        while (menuON) {
+            println menuEmpresa
+            String input = scanner.nextLine()
+
+            try {
+                int opcao = Integer.parseInt(input)
+
+                switch (opcao) {
+                    case 0:
+                        menuON = false
+                        break
+                    case 1:
+                        controllerEmpresa.listarEmpresas()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    case 2:
+                        controllerEmpresa.cadastrarEmpresa()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    case 3:
+                        controllerEmpresa.alterarEmpresa()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    case 4:
+                        controllerEmpresa.deletarEmpresa()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    default:
+                        println("Erro: opção inválida!")
+                        break
+                }
+
+            } catch (NumberFormatException e) {
+                System.err.println("Erro: entrada inválida! Por favor, insira um número.")
+            } catch (IOException e) {
+                System.err.println("Erro no menu empresas: " + e.getMessage())
+            }
+        }
+        return menuON
+    }
 }
