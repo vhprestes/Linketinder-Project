fd765be7e57fb10dde9c94207741af96022f4550
vhprestes <vhprestes@gmail.com>
2024-10-03 18:03:42 -0300

    feat: MenuCompetencias updated looking just as all other menus

--- src/main/groovy/org/acczg/views/MenuCompetencias.groovy
+++ src/main/groovy/org/acczg/views/MenuCompetencias.groovy
@@ -1,4 +1,62 @@
 package org.acczg.views
 
+import org.acczg.utils.DivisorLinha
+
 class MenuCompetencias {
+
+    static boolean menuCompetencia() {
+        ControllerCompetencia controllerCompetencia = new ControllerCompetencia()
+        Boolean menuON = true
+        String menuCompetencia = "Por favor escolha uma opção:\n" +
+                "1. Listar Competências\n" +
+                "2. Cadastrar Competência\n" +
+                "3. Alterar Competência\n" +
+                "4. Excluir Competência\n" +
+                "0. Voltar ao menu anterior"
+
+        Scanner scanner = new Scanner(System.in)
+        while (menuON) {
+            println menuCompetencia
+            String input = scanner.nextLine()
+
+            try {
+                int opcao = Integer.parseInt(input)
+
+                switch (opcao) {
+                    case 0:
+                        menuON = false
+                        break
+                    case 1:
+                        controllerCompetencia.listarCompetencias()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    case 2:
+                        controllerCompetencia.cadastrarCompetencia()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    case 3:
+                        controllerCompetencia.alterarCompetencia()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    case 4:
+                        controllerCompetencia.deletarCompetencia()
+                        println(DivisorLinha.linha)
+                        menuON = false
+                        break
+                    default:
+                        println("Erro: opção inválida!")
+                        break
+                }
+
+            } catch (NumberFormatException e) {
+                System.err.println("Erro: entrada inválida! Por favor, insira um número.")
+            } catch (IOException e) {
+                System.err.println("Erro no menu competências: " + e.getMessage())
+            }
+        }
+        return menuON
+    }
 }
