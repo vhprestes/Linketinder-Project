375520ff7c01a99ba26db8fbf5af610d0f7689c3
vhprestes <vhprestes@gmail.com>
2024-10-03 18:50:21 -0300

    feat: update Vagas model

--- src/main/groovy/org/acczg/models/Vaga.groovy
+++ src/main/groovy/org/acczg/models/Vaga.groovy
@@ -1,4 +1,15 @@
 package org.acczg.models
 
 class Vaga {
+    int id
+    String nome
+    String descricao
+    List<String> competencias
+    String cidade
+    String empresa
+
+
+    String toString() {
+        "Vaga(id: ${id}, nome: ${nome}, descricao: ${descricao}, competencias: ${competencias}, cidade: ${cidade}, empresa: ${empresa})"
+    }
 }
