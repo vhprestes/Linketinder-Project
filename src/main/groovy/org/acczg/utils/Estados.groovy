9cdaf483e6dc0d20746dcff2ae9b210347256e84
vhprestes <vhprestes@gmail.com>
2024-10-03 15:00:55 -0300

    feat: kinda of a mock for all states and ids (alphabetical order)

---
+++ src/main/groovy/org/acczg/utils/Estados.groovy
@@ -0,0 +1,34 @@
+package org.acczg.utils
+
+public class Estados {
+static String estados =
+        (" 1. AC\n")+
+                (" 2. AL\n")+
+                (" 3. AP\n")+
+                (" 4. AM\n")+
+                (" 5. BA\n")+
+                (" 6. CE\n")+
+                (" 7. DF\n")+
+                (" 8. ES\n")+
+                (" 9. GO\n")+
+                ("10. MA\n")+
+                ("11. MT\n")+
+                ("12. MS\n")+
+                ("13. MG\n")+
+                ("14. PA\n")+
+                ("15. PB\n")+
+                ("16. PR\n")+
+                ("17. PE\n")+
+                ("18. PI\n")+
+                ("19. RJ\n")+
+                ("20. RN\n")+
+                ("21. RS\n")+
+                ("22. RO\n")+
+                ("23. RR\n")+
+                ("24. SC\n")+
+                ("25. SP\n")+
+                ("26. SE\n")+
+                ("27. TO")
+
+}
+
