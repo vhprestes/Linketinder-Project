ee284c056c6506bdf88161fc7b6826c4bba19c1f
vhprestes <vhprestes@gmail.com>
2024-10-03 18:29:10 -0300

    feat: update ControllerCompetencia to call the correct methods

--- src/main/groovy/org/acczg/controller/ControllerCompetencia.groovy
+++ src/main/groovy/org/acczg/controller/ControllerCompetencia.groovy
@@ -1,4 +1,53 @@
 package org.acczg.controller
 
+import org.acczg.models.Competencia
+import org.acczg.service.CompetenciaService
+
+
 class ControllerCompetencia {
+
+    Competencia competencia = new Competencia()
+    String novaCompetencia = ""
+
+    int opcao
+    Scanner ler = new Scanner(System.in)
+
+    void listarCompetencias() {
+        CompetenciaService.listarCompetencias()
+    }
+
+    void cadastrarCompetencia() {
+
+        println("Digite a nova competência:")
+        this.novaCompetencia = ler.nextLine()
+
+        this.competencia.setCompetencia(this.novaCompetencia)
+        CompetenciaService.cadastrarCompetencia(this.competencia)
+
+    }
+
+    void alterarCompetencia() {
+
+        CompetenciaService.listarCompetencias()
+
+        println "Escolha o código(id) da competência que deseja alterar:"
+        this.opcao = Integer.parseInt(ler.nextLine())
+
+        println("Digite a nova competência:")
+        this.novaCompetencia = ler.nextLine()
+
+        this.competencia.setId(this.opcao)
+        this.competencia.setCompetencia(this.novaCompetencia)
+        CompetenciaService.alterarCompetencia(this.competencia)
+
+    }
+
+    void deletarCompetencia() {
+
+        CompetenciaService.listarCompetencias()
+
+        println("Escolha o código(id) da competência que deseja excluir:")
+        this.opcao = Integer.parseInt(ler.nextLine())
+        CompetenciaService.deletarCompetencia(this.opcao)
+    }
 }
