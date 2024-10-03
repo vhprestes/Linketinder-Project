acbb878a521289a76cf59be44c91561a53aade02
vhprestes <vhprestes@gmail.com>
2024-10-03 19:41:00 -0300

    feat: vagasService created

---
+++ src/main/groovy/org/acczg/service/VagaService.groovy
@@ -0,0 +1,63 @@
+package org.acczg.service
+
+import org.acczg.DAO.VagaDAO
+import org.acczg.models.Vaga
+
+class VagaService {
+
+    static void listarVagas() {
+        VagaDAO vagaDAO = new VagaDAO()
+        vagaDAO.listar().each { vaga ->
+            println vaga.toString()
+        }
+    }
+
+    List<Vaga> obterVagasCadastradas() {
+        VagaDAO vagaDAO = new VagaDAO()
+        return vagaDAO.listar()
+    }
+
+    static void cadastrarVaga(Vaga vaga) {
+        VagaDAO vagaDAO = new VagaDAO()
+        boolean sucesso = vagaDAO.inserir(vaga)
+
+        if (sucesso) {
+            println "Vaga cadastrada com sucesso"
+        } else {
+            println "Erro no cadastro de vaga"
+        }
+    }
+
+    static void cadastrarCompetenciaVaga(int vagaId, int competenciaId) {
+        VagaDAO vagaDAO = new VagaDAO()
+        boolean sucesso = vagaDAO.inserirCompetenciaVaga(vagaId, competenciaId)
+
+        if (sucesso) {
+            println "Competência cadastrada com sucesso"
+        } else {
+            println "Erro no cadastro de vaga"
+        }
+    }
+
+    static void alterarVaga(Vaga vaga) {
+        VagaDAO vagaDAO = new VagaDAO()
+        boolean sucesso = vagaDAO.alterar(vaga)
+
+        if (sucesso) {
+            println "Vaga alterada com sucesso"
+        } else {
+            println "Erro na alteração de vaga"
+        }
+    }
+
+    static void deletarVaga(Integer id) {
+        VagaDAO vagaDAO = new VagaDAO()
+        boolean sucesso = vagaDAO.remover(id)
+
+        if (sucesso) {
+            println "Vaga deletada com sucesso"
+        } else {
+            println "Erro ao deletar vaga"
+        }
+    }
+}
