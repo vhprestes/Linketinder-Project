c7c6855e7271472a14cb22dbdb51df1240f7dae5
vhprestes <vhprestes@gmail.com>
2024-10-18 19:09:57 -0300

    tests: tests for empresas

--- src/test/groovy/org/acczg/utils/EmpresaUtilsTest.groovy
+++ src/test/groovy/org/acczg/utils/EmpresaUtilsTest.groovy
@@ -1,75 +1,76 @@
 package org.acczg.utils
 
-import org.acczg.entities.PessoaJuridica
+import org.acczg.DAO.EmpresaDAO
+import org.acczg.models.Empresa
+import org.acczg.service.EmpresaService
 import spock.lang.Specification
 
 class EmpresaUtilsTest extends Specification {
+    EmpresaDAO empresaDAO = Mock()
+    EmpresaService empresaService = new EmpresaService(empresaDAO)
+    PrintStream printStream = Mock()
 
-    def "test listarEmpresas"() {
-        given:
-        List<PessoaJuridica> empresas = [
-                new PessoaJuridica(nome: "Mil grau", email: "mg@mg.com", descricao: "Empresa de TI", CEP: "16600000", estado: "SP", competencias: ["Java", "Groovy"], cnpj: "12345678900", pais: "Brasil")]
-        when:
-        EmpresaUtils.listarEmpresas(empresas)
-        then:
-        empresas.size() == 1
-        empresas[0].nome == "Mil grau"
-        empresas[0].email == "mg@mg.com";
-        empresas[0].descricao == "Empresa de TI";
-        empresas[0].CEP == "16600000";
-        empresas[0].estado == "SP";
-        empresas[0].competencias == ["Java", "Groovy"];
-        empresas[0].cnpj == "12345678900";
-        empresas[0].pais == "Brasil";}
-
-
-    def testCadastroEmpresas() {
-        given:
-        List<PessoaJuridica> empresas = []
-        def expectedInputs = [
-                "Mil grau",
-                "mil@grau.com",
-                "Empresa de TI",
-                "16600000",
-                "SP",
-                "Java,Groovy",
-                "12345678900",
-                "Brasil"
-        ]
-        def inputStream = new ByteArrayInputStream(expectedInputs.join("\n").getBytes())
-        System.setIn(inputStream) // Set simulated input stream
-
-        when:
-        EmpresaUtils.cadastroEmpresas(empresas)
-
-        then:
-        empresas.size() == 1
-        empresas[0].nome == expectedInputs[0]
-        empresas[0].email == expectedInputs[1]
-        empresas[0].descricao == expectedInputs[2]
-        empresas[0].CEP == expectedInputs[3]
-        empresas[0].estado == expectedInputs[4]
-        empresas[0].competencias == ["Java", "Groovy"]
-        empresas[0].cnpj == expectedInputs[6]
-        empresas[0].pais == expectedInputs[7]
-
-
+    def setup() {
+        System.setOut(printStream)
     }
 
-    def testCadastroEmpresasError() {
+    def "Cadastrar Empresa should print success message"() {
         given:
-        List<PessoaJuridica> empresas = []
-        def expectedInputs = [
-                "Mil grau"]
-        def inputStream = new ByteArrayInputStream(expectedInputs.join("\n").getBytes())
-        System.setIn(inputStream) // Set simulated input stream
+        Empresa empresa = new Empresa(id: 1, nome: "Empresa X", cnpj: "12345678000100", email: "x@y.com", senha: "senha", pais: "1", estado: "17", cep: "12345678", descricao: "Empresa X")
 
         when:
-        EmpresaUtils.cadastroEmpresas(empresas)
+        empresaService.cadastrarEmpresa(empresa)
 
         then:
-//        expect NoSuchElementException
-        empresas.size() == 0
+        1 * empresaDAO.inserir(empresa)
+        1 * printStream.println("Empresa cadastrada com sucesso")
     }
 
+    def "Cadastrar Empresa should throw exception on failure"() {
+        given:
+        Empresa empresa = null
+
+        when:
+        empresaService.cadastrarEmpresa(empresa)
+
+        then:
+        0 * printStream.println("Empresa cadastrada com sucesso")
+    }
+
+    def "Alterar Empresa should print success message"() {
+        given:
+        Empresa empresa = new Empresa(id: 1, nome: "Empresa X", cnpj: "12345678000100", email: "x@y.com", senha: "senha", pais: "1", estado: "17", cep: "12345678", descricao: "Empresa X")
+
+        when:
+        empresaService.alterarEmpresa(empresa)
+
+        then:
+        1 * empresaDAO.alterar(empresa)
+        1 * printStream.println("Empresa alterada com sucesso")
+    }
+
+    def "Deletar Empresa should print success message"() {
+        given:
+        Integer id = 1
+
+        when:
+        empresaService.deletarEmpresa(id)
+
+        then:
+        1 * empresaDAO.remover(id)
+        1 * printStream.println("Empresa deletada com sucesso")
+    }
+
+    def "Deletar Empresa should throw exception on failure"() {
+        given:
+        Integer id = 1
+        empresaDAO.remover(id) >> { throw new Exception("Erro ao tentar remover a empresa. Erro: erro") }
+
+        when:
+        empresaService.deletarEmpresa(id)
+
+        then:
+        thrown(Exception)
+        0 * printStream.println("Empresa removida com sucesso")
+    }
 }
